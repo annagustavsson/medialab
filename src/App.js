c0b4355f6d24eeabe3138ce3b7506987c50ec5b4
Kristina Andersson <kristina-andersson@outlook.com>
2021-05-03 23:17:49 +0200

    replace global css with scss and add _Colors.scss to assets

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
-import FirebaseContextProvider from "./contexts/FirebaseContext"
-import HomePage from "./pages/HomePage/HomePage"
-import SideBar from "./components/Sidebar/SiderBar"
-import ForumPage from "./pages/ForumPage/ForumPage"
-import TurnHerePage from "./pages/TurnHerePage/TurnHerePage"
-import ResourcesPage from "./pages/ResourcesPage/ResourcesPage"
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FirebaseContextProvider from './contexts/FirebaseContext';
+import HomePage from './pages/HomePage/HomePage';
+import SideBar from './components/Sidebar/SiderBar';
+import ForumPage from './pages/ForumPage/ForumPage';
+import TurnHerePage from './pages/TurnHerePage/TurnHerePage';
+import ResourcesPage from './pages/ResourcesPage/ResourcesPage';
 
 function App() {
-  return (
-    <FirebaseContextProvider>
-      <Router>
-        <Switch>
-          <div>
-          <SideBar/>
-          <Route path="/" exact render={() => <HomePage/>}/>
-          <Route path="/forum" exact render={() => <ForumPage/>}/>
-          <Route path="/wheretoturn" exact render={() => <TurnHerePage/>}/>
-          <Route path="/resources" exact render={() => <ResourcesPage/>}/>
-          </div>
-        </Switch>
-      </Router>
-    </FirebaseContextProvider>
-    
-  );
+	return (
+		<FirebaseContextProvider>
+			<Router>
+				<Switch>
+					<div className='content'>
+						<SideBar />
+						<Route path='/' exact render={() => <HomePage />} />
+						<Route path='/forum' exact render={() => <ForumPage />} />
+						<Route path='/wheretoturn' exact render={() => <TurnHerePage />} />
+						<Route path='/resources' exact render={() => <ResourcesPage />} />
+					</div>
+				</Switch>
+			</Router>
+		</FirebaseContextProvider>
+	);
 }
 
 export default App;
