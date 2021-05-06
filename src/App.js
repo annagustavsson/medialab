499cb6c3f02eb637eac9ef6ff49649d493e6ea5f
Kristina Andersson <kristina-andersson@outlook.com>
2021-05-06 23:26:33 +0200

    add colors, fonts, and other assets

--- src/App.js
+++ src/App.js
@@ -6,17 +6,34 @@ import ForumPage from './pages/ForumPage/ForumPage';
 import TurnHerePage from './pages/TurnHerePage/TurnHerePage';
 import ResourcesPage from './pages/ResourcesPage/ResourcesPage';
 
+import logo from './assets/images/KTH.png';
+
 function App() {
 	return (
 		<FirebaseContextProvider>
 			<Router>
 				<Switch>
-					<div className='content'>
-						<Sidebar />
-						<Route path='/' exact render={() => <HomePage />} />
-						<Route path='/forum' exact render={() => <ForumPage />} />
-						<Route path='/wheretoturn' exact render={() => <TurnHerePage />} />
-						<Route path='/resources' exact render={() => <ResourcesPage />} />
+					<div className='app-container'>
+						<div className='content'>
+							<Sidebar />
+							<main className='main'>
+								<Route path='/' exact render={() => <HomePage />} />
+								<Route path='/forum' exact render={() => <ForumPage />} />
+								<Route
+									path='/wheretoturn'
+									exact
+									render={() => <TurnHerePage />}
+								/>
+								<Route
+									path='/resources'
+									exact
+									render={() => <ResourcesPage />}
+								/>
+							</main>
+						</div>
+						<div className='logoContainer'>
+							<img src={logo} alt='logo' />
+						</div>
 					</div>
 				</Switch>
 			</Router>
